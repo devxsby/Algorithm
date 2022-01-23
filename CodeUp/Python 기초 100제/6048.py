cc109898c45e9ec8c01305a2376d7bd6160f0c69
sese3211 <sese3211@gmail.com>
2022-01-23 17:39:58 +0900

    update

---
+++ CodeUp/Python 기초 100제/6048.py
@@ -0,0 +1,9 @@
+# 6048 : [기초-비교연산] 정수 2개 입력받아 비교하기1(설명)(py)
+a, b = map(int,input().split())
+''' 내풀이
+if a < b:
+    print("True")
+else: print("False")
+'''
+
+print(a<b) # 비교연산자는 결과를 true, false로 바로 계산해줌
