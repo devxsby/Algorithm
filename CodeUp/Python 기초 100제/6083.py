ddba23333f7b545ecfac0c5c79608858b1c5fc04
sese3211 <sese3211@gmail.com>
2022-01-29 12:47:28 +0900

    update

--- CodeUp/Python 기초 100제/6083.py
+++ CodeUp/Python 기초 100제/6083.py
@@ -1,7 +1,7 @@
 # 6083 : [기초-종합] 빛 섞어 색 만들기(설명)(py)
-a, b, c = map(int,input().split())
-for x in range (0,a):
-    for y in range (0,b):
-        for z in range (0,c):
+r, g, b = map(int,input().split())
+for x in range (0,r):
+    for y in range (0,g):
+        for z in range (0,b):
             print(x,y,z)
-print(a*b*c)
+print(r*g*b)
