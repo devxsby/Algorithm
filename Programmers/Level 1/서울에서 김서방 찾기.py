525f2fd212dff4a69c0a2af5a9bbfccf01feac76
sese3211 <sese3211@gmail.com>
2022-02-05 22:41:36 +0900

    update

--- Programmers/Level 1/서울에서 김서방 찾기.py
+++ Programmers/Level 1/서울에서 김서방 찾기.py
@@ -4,3 +4,7 @@ def solution(seoul):
         if seoul[i] == "Kim":
             answer = '김서방은 {}에 있다'.format(i)
     return answer
+
+# 다른 풀이
+# def findKim(seoul):
+#    return "김서방은 {}에 있다".format(seoul.index('Kim'))
