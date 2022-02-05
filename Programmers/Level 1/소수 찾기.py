65cf51cc1d89b337fbf41448b53dfbefa4240ebc
sese3211 <sese3211@gmail.com>
2022-02-05 23:11:09 +0900

    update

--- Programmers/Level 1/소수 찾기.py
+++ Programmers/Level 1/소수 찾기.py
@@ -1,22 +1,10 @@
 # 소수 찾기
+# 효율성 통과 못해서 다른 풀이 갖고 옴 
+# 모범 답안 : : 에라토스테네스의 체(Sieve of Eratosthenes)
 def solution(n):
-    count = 0
-    for n in range(2, n+1):
-        for i in range(2, n):
-            if n%i == 0: # 소수 아님
-                break
-        else:    
-            count += 1
-    return count
-
-# 다른 풀이 : 에라토스테네스의 체(Sieve of Eratosthenes)
-'''
-def solution(n):
-    num = set(range(2,n+1))
+    answer = set(range(2,n+1))
     
     for i in range(2,n+1):
-        if i in num:
-            num -= set(range(2*i, n+1, i))
- 
-    return len(num)
-'''
+        if i in answer:
+            answer -= set(range(i*2,n+1,i))
+    return len(answer)
