9e19e65c75f773e20ff8d9744bbd6a910b4b93a8
Subin Yoon <80062632+sese3211@users.noreply.github.com>
2022-03-05 23:18:46 +0900

    update

---
+++ Programmers/Level 1/부족한 금액 계산하기.py
@@ -0,0 +1,10 @@
+# 부족한 금액 계산하기
+def solution(price, money, count):
+    result = 0
+    for i in range(1,count+1):
+        result += price * i
+    if result >= money:
+        answer = result -money
+    else:
+        answer = 0
+    return answer
