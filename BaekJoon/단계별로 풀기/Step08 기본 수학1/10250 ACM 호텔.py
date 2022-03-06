032beeabba2689a0b29bf72c08ee2328283bc14a
Subin Yoon <80062632+sese3211@users.noreply.github.com>
2022-03-06 11:59:48 +0900

    update

--- BaekJoon/단계별로 풀기/Step08 기본 수학1/10250 ACM 호텔.py
+++ BaekJoon/단계별로 풀기/Step08 기본 수학1/10250 ACM 호텔.py
@@ -1,4 +1,9 @@
 t = int(input())
 for _ in range(t):
     h, w, n = map(int,input().split()) # 층 수, 방 수, 몇 번째 손님
-    
+    num = n//h + 1
+    floor = n % h
+    if n % h == 0:  # h의 배수이면,
+        num = n//h
+        floor = h
+    print(f'{floor*100+num}')
