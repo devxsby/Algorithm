c6232c666edd08fdf4873f5406d5eeda976aacae
devxsby <devxsby@gmail.com>
2022-04-20 00:55:03 +0900

    update

--- BaekJoon/단계별로 풀기/Step12 정렬/10814 나이순 정렬.py
+++ BaekJoon/단계별로 풀기/Step12 정렬/10814 나이순 정렬.py
@@ -1,5 +1,12 @@
 N = int(input())
 member_list = []
 
-for i in range(N):
-    member_list.append(i)
+for _ in range(N):
+    age, name = map(str, input().split())
+    age = int(age)
+    member_list.append((age, name))
+
+member_list.sort(key = lambda x : x[0])	# (age, name)에서 age만 비교
+
+for i in member_list:
+    print(i[0], i[1])
