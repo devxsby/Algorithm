2bb0f025312e08deebddb7fd701267f0c5badc9f
devxsby <devxsby@gmail.com>
2022-04-21 00:03:07 +0900

    update

--- BaekJoon/단계별로 풀기/Step12 정렬/1181 단어 정렬.py
+++ BaekJoon/단계별로 풀기/Step12 정렬/1181 단어 정렬.py
@@ -3,8 +3,8 @@ word_list= []
 
 for i in range(N):
     word_list.append(input())
-set_lst = set(word_list)	## set으로 변환해서 중복값을 제거
-word = list(set_lst)	## 다시 word_list로 변환
+set_list = set(word_list)	# set으로 변환해서 중복값을 제거
+word_list = list(set_list)	# 다시 word_list로 변환
 word_list.sort()
 word_list.sort(key = len)
 
